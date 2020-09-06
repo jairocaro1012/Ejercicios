5f363bd2bc90c465e9168b7c3179cf74d4d96832
Johan <johans10298@hotmail.com>
2020-09-05 20:56:14 -0500

    punto1

---
+++ Edad.py
@@ -0,0 +1,24 @@
+class edad:
+
+    a=1
+    b=2
+    print("Ingresa tu edad")
+    global ed
+    ed = int(input())
+    print("Ingresa el año actual")
+    global an
+    an = int(input())
+
+    def E2070():
+        da=2070-an
+        ned = ed+da
+        print("Tu edad sera", ned)
+        return ned
+
+    E2070()
+
+
+
+
+
+
